50ea79ce36ac4da0884696bd9cbee8fbc42f49d7
MaximkaProMax <Zholdashev.Maks@yandex.ru>
2023-11-22 11:52:07 +0300

    Add code

--- register.php
+++ register.php
@@ -2,25 +2,60 @@
 
 $servername = "localhost";
 $username = "root";
-$password = "";
-$dbname ="training";
+$password = "3";
+$dbname = "training";
 
-$conn = new mysqli($servername, $username, $password, $dbname) ;
-    if ($conn->connect_error) {
-        die("connect failed: " . $conn->connect_error);
-    }
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("connect failed: " . $conn->connect_error);
+}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $ POST["name"];
-    $email = $ POST["email"];
-    $password = password_hash($_POST["password"], PASSWORD DEFAULT) ;
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
-$query = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
+    $query = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
 
     if ($conn->query($query) === TRUE) {
-
-    echo "Вы зарегестрированы";
+        echo "Вы зарегестрированы";
     } else {
-    echo “Oww6Ka: “ . $conn->error;
+        echo "Ошибка: " . $conn->error;
     }
 }
+
+?>
+
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Peructpauma</title>
+    </head>
+<body>
+
+<div class="container">
+    <h2>Peructpauma</h2>
+
+<form action="register.php" method="post">
+    <label for="name">Uma:</label>
+    <input type="text" id="name" name="name" required>
+
+    <label for="email">Nouta:</label>
+    <input type="email" id="email" name="email" required>
+
+    <label for="password">Maponb:</label>
+    <input type="password" id="password" name="password" required>
+
+    <button type="submit">3apeructpupoBatbca</button>
+</form>
+</div>
+
+</body>
+</html>
